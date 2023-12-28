505337029a7c5a1447933e327539739d2e331f55
Agoyal011 <eng.aniketgoyal@gmail.com>
2023-12-28 16:59:25 -0500

    Added all the operators with examples

--- script.js
+++ script.js
@@ -5,3 +5,70 @@
 console.log("Hello World")
 
 // Operators in Js 
+
+let a=5;
+let b=9;
+console.log(a+b);
+console.log(a%b);
+
+// Urnary Operators 
+
+// Increment 
+// Decrement
+
+let c =1; 
+c++
+
+console.log(c);
+
+{
+    let a =20;
+    a-- 
+    console.log(a); 
+
+}
+
+
+// Comparison Operators (will return True or False)
+
+// This is start of a block 
+{
+ let a =5 ;
+ let b ="5";
+ 
+ console.log('a==b', a==b); // Will return True 
+
+ console.log("a==b", a===b);// Will return false ( Triple equal to compare the data types first) 
+
+}
+
+
+// Logical Operators 
+
+// Logical &&    // BOth the conditions should be true to return true
+
+// For example
+{
+
+    let a =6 ;
+    let b= 5; 
+
+    const cond1 = a>b;// True 
+    const cond2= a===6;// True
+
+    console.log("Cond1 and Cond2",cond1&& cond2);
+}
+// Logical OR || // If any one of them is True, it will return True 
+// Logical NOT ! // Will make true condition false and vice versa 
+
+{
+
+    let  a =6 ; 
+    let b= 12; 
+
+    const cond1= a<b; // True 
+
+    console.log("Condition is", !cond1)
+};
+
+
