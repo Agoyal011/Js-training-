1e058ed527f94b1fac763a3df0d32aac882ec3c1
Agoyal011 <eng.aniketgoyal@gmail.com>
2023-12-28 14:45:17 -0500

    Variables and data types

---
+++ first.js
@@ -0,0 +1,41 @@
+fullname="Tony Stark";
+console.log(fullname);
+
+isFollow=true;
+console.log(isFollow);
+
+{
+    let a =10;
+    console.log(a)
+}
+
+{
+    let a= 20;
+    console.log(a)
+}
+
+const student={
+
+    name:"Aniket Goyal",
+    age: 24,
+    Company:"Wenglor Sensoric Group Canada Inc"
+
+};
+
+const person1={
+
+username:"AayushBajaj",
+isFollow:true,
+posts: 195,
+followers: "595"+ "k", 
+following: 4,
+profession: "Enterprenuer", 
+Quote: "To educate somenone is the highest prevelige"
+
+};  
+
+// This is a single line comment 
+/* This is a multi line comment */
+
+
+
