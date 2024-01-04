e703b8fabdced4e932cec1b0e3b249d044a68846
Agoyal011 <eng.aniketgoyal@gmail.com>
2024-01-04 16:15:51 -0500

    Loops completed

--- loops.js
+++ loops.js
@@ -1,4 +1,43 @@
-for(i=1;i<=5;i++)
+// This script will focus on loops
+// Used to execute a piece of information again and again. 
+
+for(let i=1;i<=5;i++)
 {
     console.log("Apna college");
 }
+
+
+// Calculate sum of 1 to 5 
+
+let sum=0;
+for ( let j=1; j<=5;j++)
+
+{
+    
+    sum=sum+j;
+}
+    console.log(sum);
+
+
+// While loop ( initialized first, check the condition, increment when the loop begins)
+
+let i=10;
+while(i <=20)
+
+{
+    console.log( "i values are : ", i);
+    i++; 
+}
+
+// example of a do-while loop  ( initialization, statmennt, increment, condition)
+
+// EXCEPTION 
+
+// Loop will atleast run once
+
+let d=20
+do { 
+console.log("My name is Aniket ")
+d++
+}
+while (d<=30)
